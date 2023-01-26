77a0223106c0516371a441cd105d89a797fc01ed
Emre Yesildag <e12120490@student.tuwien.ac.at>
2023-01-26 18:11:58 +0100

    Initial commit

---
+++ src/main/java/com/example/slmtestquadratyesildag/SlmTestQuadratYesildagApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmtestquadratyesildag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestQuadratYesildagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmTestQuadratYesildagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slmtestquadratyesildag/SlmTestQuadratYesildagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmtestquadratyesildag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestQuadratYesildagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
